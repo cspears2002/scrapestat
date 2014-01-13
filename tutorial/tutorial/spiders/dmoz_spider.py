347941e3992683c38dee6deb5e38dc0c45115c3b
cspears2002 <cspears2002@yahoo.com>
2014-01-13 12:46:43 -0800

    Made changes to spider to pull out book titles.

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,4 +1,5 @@
 from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
 
 class DmozSpider(BaseSpider):
   name = "dmoz"
@@ -14,5 +15,10 @@ class DmozSpider(BaseSpider):
     return cls(spider)
 
   def parse(self, response):
-    filename = response.url.split("/")[-2]
-    open(filename, 'wb').write(response.body)
+    sel = Selector(response)
+    sites = sel.xpath('//ul/li')
+    for site in sites:
+      title = site.xpath('a/text()').extract()
+      link = site.xpath('a/@href').extract()
+      desc = site.xpath('text()').extract()
+      print title, link, desc
