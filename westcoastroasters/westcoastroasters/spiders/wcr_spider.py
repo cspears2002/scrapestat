ac280cf69152ba82dfd8ff6534f0205fc1f16483
cspears2002 <cspears2002@yahoo.com>
2014-01-13 19:16:09 -0800

    Managed to scrape off the bean names and prices.

--- westcoastroasters/westcoastroasters/spiders/wcr_spider.py
+++ westcoastroasters/westcoastroasters/spiders/wcr_spider.py
@@ -1,9 +1,15 @@
 from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
 
 class WCRSpider(BaseSpider):
   name = "wcr"
   start_urls = ["http://www.westcoastroasting.com"]
 
   def parse(self, response):
-    filename = "beans.txt"
-    open(filename, 'wb').write(response.body)
+    sel = Selector(response)
+    top_sellers = sel.xpath('//div[@id="SideTopSellers"]/div[@class="BlockContent"]/ul/li/div[@class="ProductDetails"]')
+    bean_names = top_sellers.xpath('strong/a/text()').extract()
+    bean_prices = top_sellers.xpath('div[@class="ProductPriceRating"]/em/text()').extract()
+    print bean_names, bean_prices
+# top_sellers_names = sel.xpath('//div[@id="SideTopSellers"]/div[@class="BlockContent"]/ul/li/div[@class="ProductDetails"]/strong/a/text()').extract()
+# top_sllers_prices = sel.xpath('//div[@id="SideTopSellers"]/div[@class="BlockContent"]/ul/li/div[@class="ProductDetails"]/div[@class="ProductPriceRating"]/em/text()').extract()
